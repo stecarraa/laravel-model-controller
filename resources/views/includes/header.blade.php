6377c4a0c11f8ca1b2a96232cc75d1cd62828382
stecarraa <stefano@caratozzolo.me>
2022-09-08 17:16:15 +0200

    feat : css and route ok

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,29 @@
-<footer>
-    <h2>
-        Questo è un header
-    </h2>
-</footer>
+<header>
+   
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Boolean Movies Database</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('movies')}}">Movies</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</header>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,26 +3,27 @@
 @section("title", "Movies")
 
 @section("main-content")
-   <h2>card film</h2>
+  <div class="container text-center">
+    <h2>Movies</h2>
 
-   @forelse ($movies as $movie)
+    @forelse ($movies as $movie)
 
-   <div>
-       <h3>
-           Title: {{ $movie->title }} - {{ $movie->date }}
-       </h3>
-       <h4>
-           Original title: {{ $movie->original_title }} - {{ $movie->language }}
-       </h4>
-       <h4>
-           Vote: {{ $movie->vote }}
-       </h4>
 
+
+   <div class="container">
+    <div class="card border border-primary shadow-0 mb-3">
+        <div class="card-header">Title: {{ $movie->title }} - {{ $movie->date }}</div>
+        <div class="card-body text-primary">
+          <h5 class="card-title">Vote: {{ $movie->vote }}</h5>
+          <p class="card-text"> Original title: {{ $movie->original_title }} - {{ $movie->nationality }}</p>
+        </div>
+      </div>
    </div>
-
-   @empty
-       <h4>No movies found</h4>
-
-   @endforelse
+ 
+    @empty
+        <h4>No movies found</h4>
+ 
+    @endforelse
+  </div>
    
 @endsection
